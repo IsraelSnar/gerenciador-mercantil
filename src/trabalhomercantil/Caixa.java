5dabab5835349787f475f0b9547c06c399723bb3
João Alves <38361266+joaoalvesxp@users.noreply.github.com>
2022-02-03 04:31:36 -0300

    caixa falta implementar a sangria

--- src/trabalhomercantil/Caixa.java
+++ src/trabalhomercantil/Caixa.java
@@ -1,5 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package trabalhomercantil;
 
+/**
+ *
+ * @author João Alves
+ */
 public class Caixa {
+    private float saldoCaixa;
+
+    public Caixa(float saldoCaixa) {
+        this.saldoCaixa = saldoCaixa;
+    }
+    
+    public void sangria(float valor){
+        setSaldoCaixa(saldoCaixa - valor);
+    }
+
+    public float getSaldoCaixa() {
+        return saldoCaixa;
+    }
+    
+    public void setSaldoCaixa(float saldoCaixa) {
+        this.saldoCaixa = saldoCaixa;
+    }
+    
+    
     
 }
